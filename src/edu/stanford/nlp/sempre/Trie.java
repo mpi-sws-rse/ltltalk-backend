b40c734775523170965e5a29899259ecf61572fd
Akshal Aniche <aniche.akshal@gmail.com>
2018-07-02 15:32:46 +0200

    Added rule removal and cleanup from a Trie

--- src/edu/stanford/nlp/sempre/Trie.java
+++ src/edu/stanford/nlp/sempre/Trie.java
@@ -2,6 +2,7 @@ package edu.stanford.nlp.sempre;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
 import java.util.HashMap;
@@ -9,7 +10,7 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * Used to access rules efficiently by walking down their RHS.
+ * Used to access or delete rules efficiently by walking down their RHS.
  * Stores all rules we are currently using
  * @author Percy Liang
  */
@@ -24,6 +25,7 @@ public class Trie {
   public Trie next(String item) { return children.get(item); }
 
   public void add(Rule rule) { add(rule, 0); }
+  
   private void add(Rule rule, int i) {
     cats.add(rule.lhs);
 
@@ -38,6 +40,81 @@ public class Trie {
     if (child == null)
       children.put(item, child = new Trie());
     child.add(rule, i + 1);
+  }
+
+  /** Method to delete a rule from the Trie
+   * @author Akshal Aniche
+   * @param rule
+   */
+  public void remove(Rule rule){ remove(rule, 0); }
+  
+  /** Helper method to recursively traverse the trie and delete the specified rule with clean up in a bottom up strategy
+   * @author Akshal Aniche
+   * @param rule
+   * @param i
+   */
+  private void remove(Rule rule, int i) {
+	  //filter and delete exact match
+	  if (i == rule.rhs.size()) {
+		  if (rules.contains(rule)) {
+			  rules.remove(rule);
+		  } else {
+			  throw new IllegalArgumentException("You can't delete a rule that doesn't exist.");
+		  }
+	  }
+	  else {
+		  String item = rule.rhs.get(i);
+		  Trie child = children.get(item);
+		  if (child == null) {
+			  throw new IllegalArgumentException("You can't delete a rule that doesn't exist.");
+		  }
+		  child.remove(rule, i + 1);
+		  
+		  if(child.isEmpty()) { //All of the rules in child and its descendant have been deleted
+			  children.remove(item); //delete the child from the trie
+		  }
+	  }
+	  if (!catExists(rule.lhs)) { //remove the lhs of the rule from cats if that was the only rule of the specified category
+		  cats.remove(rule.lhs);
+	  }
+	  return;
+	  
+  }
+  
+  /** Check whether a rule in the Trie has the same lhs as the given String (i.e. is of the given category)
+   * @author Akshal Aniche
+   * @param cat
+   * @return true iff there exists a rule in rules with lhs==cat or there exists a rule in children with lhs==cat 
+   */
+  private boolean catExists(String cat) {
+	  Iterator<Rule> iter = rules.iterator();  
+	  while (iter.hasNext()) {
+		  Rule rule = iter.next();
+		  if (cat.equals(rule.lhs)) { 	//there is a rule of the given category
+			  return true;		
+		  }
+	  }
+	  boolean result = false; 			//there is no rule of the given category in the ArrayList
+	  
+	  //Need to check if the category exists in one of the children
+	  for (Trie child : children.values()) {
+		  result = result || child.catExists(cat); //compile the result from all the descendants
+	  }
+	  
+	  return result;
+  }
+  
+
+  /** Checks whether a Trie is empty 
+   * @author Akshal Aniche
+   * @return true iff the ArrayList of rules of the Trie is empty and the arrayList of rules of each of its descendant is empty
+   */
+  public boolean isEmpty() {
+	  boolean result = this.rules.isEmpty();	//initial choice
+	  for (Trie child : children.values()) {
+		  result = result && child.isEmpty(); 	//compile the result from all the descendants
+	  }
+	  return result;
   }
   
   public String toString(){
